df352ad72ac5d48c358d654a12da305622d47656
ram-kamineni <kamineniramuchowdary@gmail.com>
2025-04-16 14:55:16 +0530

    deleted

--- introduction/index.js
+++
@@ -1,4 +0,0 @@
-const obj = require("./main.js");
-console.log("hello this is my 1st node project");
-console.log("hello");
-console.log(obj.fun1);
--- introduction/main.js
+++
@@ -1,5 +0,0 @@
-function sample(){
-    console.log("ram kamineni")
-    return "chowdary"
-}
-module.exports = {fun1 : sample()}
